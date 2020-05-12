d71c638cf13416ba7545dfa9ce9c29362693984c
l0stpenguin <>
2020-05-12 21:01:32 +0400

    re added nyu code

--- data.py
+++ data.py
@@ -84,9 +84,8 @@ class RedWeb_BasicAugmentRGBSequence(Sequence):
             index = min((idx * self.batch_size) + i, self.N-1)
 
             sample = self.dataset[index]
-            
-            rgb_sample = cv2.imdecode(np.asarray(self.data['x/{}'.format(sample)]), 1)
-            depth_sample = self.data['y/{}'.format(sample)] 
+            rgb_sample = cv2.imdecode(np.asarray(self.data['{}'.format(sample)]), 1)
+            depth_sample = self.data['{}'.format(sample)] 
             depth_sample = resize(depth_sample, (self.shape_depth[1], self.shape_depth[2]), preserve_range=True, mode='reflect', anti_aliasing=True )
             
             x = np.clip(rgb_sample/255, 0, 1)
@@ -101,3 +100,80 @@ class RedWeb_BasicAugmentRGBSequence(Sequence):
             #self.policy.debug_img(batch_x[i], np.clip(DepthNorm(batch_y[i],self.maxDepth)/self.maxDepth,0,1), index, i)
 
         return batch_x, batch_y
+
+class NYU_BasicAugmentRGBSequence(Sequence):
+    def __init__(self, data, dataset, batch_size, shape_rgb, shape_depth, is_flip=False, is_addnoise=False, is_erase=False):
+        self.data = data
+        self.dataset = dataset
+        self.policy = BasicPolicy( color_change_ratio=0.50, mirror_ratio=0.50, flip_ratio=0.0 if not is_flip else 0.2, 
+                                    add_noise_peak=0 if not is_addnoise else 20, erase_ratio=-1.0 if not is_erase else 0.5)
+        self.batch_size = batch_size
+        self.shape_rgb = shape_rgb
+        self.shape_depth = shape_depth
+        self.maxDepth = 1000.0
+
+        from sklearn.utils import shuffle
+        self.dataset = shuffle(self.dataset, random_state=0)
+
+        self.N = len(self.dataset)
+
+    def __len__(self):
+        return int(np.ceil(self.N / float(self.batch_size)))
+
+    def __getitem__(self, idx, is_apply_policy=True):
+        batch_x, batch_y = np.zeros( self.shape_rgb ), np.zeros( self.shape_depth )
+
+        # Augmentation of RGB images
+        for i in range(batch_x.shape[0]):
+            index = min((idx * self.batch_size) + i, self.N-1)
+
+            sample = self.dataset[index]
+
+            x = np.clip(np.asarray(Image.open( BytesIO(self.data[sample[0]]) )).reshape(480,640,3)/255,0,1)
+            y = np.clip(np.asarray(Image.open( BytesIO(self.data[sample[1]]) )).reshape(480,640,1)/255*self.maxDepth,0,self.maxDepth)
+            y = DepthNorm(y, maxDepth=self.maxDepth)
+
+            batch_x[i] = nyu_resize(x, 480)
+            batch_y[i] = nyu_resize(y, 240)
+
+            if is_apply_policy: batch_x[i], batch_y[i] = self.policy(batch_x[i], batch_y[i])
+
+            # DEBUG:
+            #self.policy.debug_img(batch_x[i], np.clip(DepthNorm(batch_y[i])/maxDepth,0,1), idx, i)
+        #exit()
+
+        return batch_x, batch_y
+
+class NYU_BasicRGBSequence(Sequence):
+    def __init__(self, data, dataset, batch_size,shape_rgb, shape_depth):
+        self.data = data
+        self.dataset = dataset
+        self.batch_size = batch_size
+        self.N = len(self.dataset)
+        self.shape_rgb = shape_rgb
+        self.shape_depth = shape_depth
+        self.maxDepth = 1000.0
+
+    def __len__(self):
+        return int(np.ceil(self.N / float(self.batch_size)))
+
+    def __getitem__(self, idx):
+        batch_x, batch_y = np.zeros( self.shape_rgb ), np.zeros( self.shape_depth )
+        for i in range(self.batch_size):            
+            index = min((idx * self.batch_size) + i, self.N-1)
+
+            sample = self.dataset[index]
+
+            x = np.clip(np.asarray(Image.open( BytesIO(self.data[sample[0]]))).reshape(480,640,3)/255,0,1)
+            y = np.asarray(Image.open(BytesIO(self.data[sample[1]])), dtype=np.float32).reshape(480,640,1).copy().astype(float) / 10.0
+            y = DepthNorm(y, maxDepth=self.maxDepth)
+
+            batch_x[i] = nyu_resize(x, 480)
+            batch_y[i] = nyu_resize(y, 240)
+
+            # DEBUG:
+            #self.policy.debug_img(batch_x[i], np.clip(DepthNorm(batch_y[i])/maxDepth,0,1), idx, i)
+        #exit()
+
+        return batch_x, batch_y
+
