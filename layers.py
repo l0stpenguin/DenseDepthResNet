ab4c442d66dcd3760b531b04e48bb375d6bdca0f
l0stpenguin <17478561+l0stpenguin@users.noreply.github.com>
2020-05-28 11:19:27 +0400

    fixed resize_images for tensorflow v2

--- layers.py
+++ layers.py
@@ -34,8 +34,8 @@ class BilinearUpSampling2D(Layer):
         elif self.data_format == 'channels_last':
             height = self.size[0] * input_shape[1] if input_shape[1] is not None else None
             width = self.size[1] * input_shape[2] if input_shape[2] is not None else None
-        
-        return tf.image.resize_images(inputs, [height, width], method=tf.image.ResizeMethod.BILINEAR, align_corners=True)
+        return tf.image.resize(inputs, [height, width], method=tf.image.ResizeMethod.BILINEAR, align_corners=True)        
+#         return tf.image.resize_images(inputs, [height, width], method=tf.image.ResizeMethod.BILINEAR, align_corners=True)
 
     def get_config(self):
         config = {'size': self.size, 'data_format': self.data_format}
